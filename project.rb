11d2ceba01684c4e9461f5f8ce9eb1331cad32b0
mbernhard7494 <mbernhard7494@tamu.edu>
2016-10-20 09:40:00 -0500

    Update and rename start.rb to project.rb

--- project.rb
+++ project.rb
@@ -1,12 +1,15 @@
 require 'date'
 require 'open-uri'
 #Then we want to be able to grab the file into the program
+File.open (http://s3.amazonaws.com/tcmg412-fall2016/http_access_log)
 
 
-
-
+#variables
 num = 0
 each_file = ()
 perday = ()
 errors = ()
 
+File.foreach(http_access_log) do |line|
+  num = num + 1
+  
