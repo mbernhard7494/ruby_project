14512f6569a3c719283b7b4956140de5b78c89cf
mbernhard7494 <mbernhard7494@tamu.edu>
2016-10-21 07:58:22 -0500

    Update project.rb

--- project.rb
+++ project.rb
@@ -4,6 +4,7 @@ require 'open-uri'
 download = open('http://s3.amazonaws.com/tcmg412-fall2016/http_access_log')
 IO.copy_stream (download, 'file1.file')
 #variables
+num = 0
 num_requests = 0
 requests = 0
 errors = 0
@@ -12,7 +13,7 @@ monthly_request = {}
 month = 12
 request_error = []
 re_count = {}
-
+Array1 = []
 puts 'Retrieving the file'
 
 File.readlines ('file1.file').each do |line|
@@ -22,8 +23,24 @@ File.readlines ('file1.file').each do |line|
 
   
   #need to add array of every file
-  
-  
+  if monthly_request.has_key? "#{month}"
+        monthly_request[month].push(line)
+      else
+        monthly_request[month] = Array1
+        monthly_request[month].push(line)
+      end
+
+totals_3 = 0
+totals_4 = 0
+totals_5 = 0
+counts.each do |code, count|
+	if code[0] == "3" then totals_3 += num end
+	if code[0] == "4" then totals_4 += num end
+	if code[0] == "5" then totals_5 += num end
+end
+re_pct = (totals_4.to_f / requests.to_f * 100).to_i
+pct = (totals_3.to_f / requests.to_f * 100).to_i
+		
   #error log
   if code.to_i >= 400 && code.to_i < 500
         errors = errors + 1
@@ -36,18 +53,14 @@ File.readlines ('file1.file').each do |line|
     
     
     
-    
+error_percentage = (errors.to_f / requests) * 100
+changed_percentage = (re_count.to_f / requests) * 100
     
     
  puts "\n\n\n"
 puts "-----Request Info -----"
-puts "Total number of requests: #{num_requests}
-puts "Number of requests per month: "
-monthly_requests.each do |lines|
-	puts "    
-end
-puts "Average requests per month: #{num_requests / month}
-puts "Most requested file: 
-
-puts "\n All Errors"
-puts "Encountered parsing errors on #{re_count} lines. "
+puts "\n#{re_pct.round(2)}% of requests were not successful (#{error_percentage} requests)"
+puts "#{pct.round(2)}% of requests were redirected (#{changed_percentage} requests)"
+puts "Most requested file(s): #{request.first} (requested #{most_requested_value} time)"
+puts "Least requested file(s): #{re_count.first} and #{requests.size} others (requested #{errors} time(s))"
+puts "\nSome lines may have not been parsed.  They are located in errorlog.txt"
